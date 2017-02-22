de4fb6ca9c51ec3ad4aaa4662c2a2f25f5663051
Déborah <deborah_birro@hotmail.com>
2017-02-22 17:14:56 +0100

    Título cambiado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TOMA CAMBIOH.
                 </div>
 
                 <div class="links">
